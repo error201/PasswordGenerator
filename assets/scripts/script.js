f2a8180609a3798079bdd33b9814a708c25201bd
Jason A. Arter <jason.arter@gmail.com>
2022-12-22 17:56:40 -0800

    code complete.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,18 @@
 // Assignment Code
+// Globals (yuck!)
+const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var myNumbers = ["0123456789"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var specialChar = ["!\"#$%&'()*+,-./:;<=>?@[\\]^_{|}~"]
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var password = generatePassword();
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -15,7 +20,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // Check to see if the entered data is numeric.
 function isNumeric(myString) {
-  const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   for (i = 0; i < myString.length; i++) {
     myChar = myString[i];
     if (!numerals.includes(myChar)) {
@@ -26,20 +30,58 @@ function isNumeric(myString) {
   }
 }
 
-//prompt for password criteria.
+//prompt for password length.
 function getPasswordLength() {
   var passwordLength = prompt("How many characters would you like? (8 to 128)");
   if (isNumeric(passwordLength)) {
-    console.log("password is numeric.")
     numericLength = Number(passwordLength);
     if (numericLength > 7 && numericLength < 129) {
-      console.log("password is valid number.")
+      return numericLength;
     } else {
-      console.log("password is not a valid number.")
+      return 0;
     }
   } else {
-    console.log("password is not numeric.");
+    return 0;
   }
 }
 
-getPasswordLength();
+// Main entry point of script.
+function generatePassword() {
+  var availableCharacters = [];
+  var myPasswordLength = getPasswordLength();
+  console.log(myPasswordLength);
+  var myPassword = "";
+  if (myPasswordLength == 0) {
+    return '';
+  }
+  var myLowercase = confirm("Would you like to include lower case letters?");
+  var myUppercase = confirm("Would you like to include upper case letters?");
+  var myNumeric = confirm("Would you like to include numbers?");
+  var mySpecialChars = confirm("Would you like to include special characters?");
+  if (myLowercase) {
+    availableCharacters = availableCharacters.concat(lowerCase);
+  }
+  if (myUppercase) {
+    availableCharacters = availableCharacters.concat(upperCase);
+  }
+  if (myNumeric) {
+    availableCharacters = availableCharacters.concat(myNumbers);
+  }
+  if (mySpecialChars) {
+    availableCharacters = availableCharacters.concat(specialChar);
+  }
+  availableCharacters = availableCharacters.join('');
+  if (!availableCharacters) {
+    alert("You must pick at least one type of character.")
+    return '';
+  } else {
+    passwordArray = []
+    for (i = 1; i <= myPasswordLength; i++) {
+      randomIndex = Math.floor(Math.random() * availableCharacters.length);
+      passwordArray.push(availableCharacters[randomIndex]);
+    }
+    myPassword = passwordArray.join('');
+    return myPassword;
+  }
+}
+
